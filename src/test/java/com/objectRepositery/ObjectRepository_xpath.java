0a45e8ff67a62889c9c423267093980f86f87a5e
Sihab Hossain <51927615+sihabarnab@users.noreply.github.com>
2023-11-21 15:00:16 +0600

    Add files via upload

---
+++ src/test/java/com/objectRepositery/ObjectRepository_xpath.java
@@ -0,0 +1,11 @@
+package com.objectRepositery;
+
+public class ObjectRepository_xpath {
+	
+	public static String addButtonClick="//button[contains(@class,'absolute top-0 right-0 p-2 text-white')]";
+	public static String loginButtonClick ="//span[contains(@class,'flex w-full items-center')]";
+	public static String evalyUserPhone = " //input[contains(@name,'phone')]";
+	public static String evalyUserPassword = " //input[contains(@name,'password')]";
+	public static String evalyLoginButton = "//button[contains(@type,'submit')]";
+
+}
